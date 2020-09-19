c8def17edefb98f422c97e55dcff09fdf268827e
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-19 23:41:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,22 @@
 class Triangle
   # write code here
+  attr_reader :s1, :s2, :s3
   def initialize(s1,s2,s3)
     @s1 = s1
     @s2 = s2
     @s3 = s3
   end
 
+  def kind
+    if condition
+      :equilateral
+    elsif condition
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
   class TriangleError < StandardError
    # triangle error code
  end
