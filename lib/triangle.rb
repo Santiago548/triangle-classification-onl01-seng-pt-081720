f9487fba91a66796ec3747c75e580039aa14e42c
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-19 23:33:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,12 @@
 class Triangle
   # write code here
   def initialize(s1,s2,s3)
-
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
   end
+
+  class TriangleError < StandardError
+   # triangle error code
+ end
 end
